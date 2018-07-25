63989fc9a65a36c20cfa6b1256b35bb431869bff
Damon98 <39784136+Damon98@users.noreply.github.com>
2018-07-25 13:24:06 +0800

    Create config.py

---
+++ 36kr/config.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# author Damon Tsui Time: 2018/7/25
+
+MONGO_URL = 'localhost'
+MONGO_DB = '36kr'
+MONGO_TABLE = 'news'
+
+GROUP_START = 1
+GROUP_END = 5000
+
