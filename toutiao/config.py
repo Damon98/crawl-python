f900e368ab768f11de7f5df7c95f8ce07b893ec3
Damon98 <39784136+Damon98@users.noreply.github.com>
2018-07-25 13:45:36 +0800

    Create config.py

---
+++ toutiao/config.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# author Damon Tsui Time: 2018/7/21
+
+MONGO_URL = "localhost"
+MONGO_DB = 'toutiao'
+MONGO_TABLE = 'toutiao'
+
+GROUP_START = 1
+GROUP_END = 20
+
+KEYWORD = '自然风光'
